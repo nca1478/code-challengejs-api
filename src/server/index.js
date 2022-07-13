80e930a20f74f97a735724298e559edfb94f7931
Nelson Cadenas <nca1478@yahoo.com>
2022-07-13 12:19:28 -0400

    added: first commit

---
+++ src/server/index.js
@@ -0,0 +1,46 @@
+// Dependencies
+import express from "express";
+import logger from "morgan";
+import chalk from "chalk";
+import cors from "cors";
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        // Settings App
+        this.middlewares();
+        // this.routes()
+    }
+
+    middlewares() {
+        // Cors Access
+        this.app.use(cors());
+        this.app.use(
+            logger(
+                ":method :url :status :response-time ms - :res[content-length] [:date[clf]] :remote-addr"
+            )
+        );
+
+        // Bodyparser
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: false }));
+
+        // Static Files
+        this.app.use(express.static("public"));
+    }
+
+    listen() {
+        const port = process.env.PORT;
+        this.app.listen(port, () => {
+            console.log(
+                `${chalk.yellow(
+                    "[code-challenge:API]"
+                )} Listening on port ${port}`
+            );
+        });
+    }
+}
+
+module.exports = Server;
